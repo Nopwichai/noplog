5478267476061283406d58e0840ba5f331e76e42
Nopwichai <nopwichai@gmail.com>
2024-07-25 09:40:21 +0700

    1.0.1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nopwichai/noplog
+
+go 1.22.4
---
+++ noplog.go
@@ -0,0 +1,45 @@
+package noplog
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+var log_write_time_delay int = 1
+var log_data string
+
+func Log_info(service string, fn string, msg string) {
+	log_data = log_data + time.Now().Format("15:04:05.0000") + "\tI\t" + service + "-" + fn + "\t" + msg + "\n"
+}
+func Log_warning(service string, fn string, msg string) {
+	log_data = log_data + time.Now().Format("15:04:05.0000") + "\tW\t" + service + "-" + fn + "\t" + msg + "\n"
+}
+func Log_error(service string, fn string, msg string) {
+	log_data = log_data + time.Now().Format("15:04:05.0000") + "\tE\t" + service + "-" + fn + "\t" + msg + "\n"
+}
+
+func Logs_write() {
+	dir := "./logs/"
+
+	for {
+		if len(log_data) > 0 {
+			currentDate := time.Now().Format("2006-01-02_15")
+			fname := dir + "Log-" + currentDate + ".log"
+			file, err := os.OpenFile(fname, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+			content := log_data
+			if err != nil {
+				fmt.Println("Error open file:", err)
+			} else {
+				_, err = file.WriteString(content)
+				if err != nil {
+					fmt.Println("Error writing to file:", err)
+				} else {
+					log_data = strings.Replace(log_data, content, "", -1)
+				}
+			}
+		}
+		time.Sleep(time.Duration(log_write_time_delay) * 1000 * time.Millisecond)
+	}
+}
