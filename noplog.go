93de5b054d1daeb281ae9e25d417c599c90ce066
Nopwichai <nopwichai@gmail.com>
2024-07-25 13:44:09 +0700

    Update noplog.go

--- noplog.go
+++ noplog.go
@@ -7,26 +7,28 @@ import (
 	"time"
 )
 
-var log_data string
+var log_data_info string
+var log_data_warning string
+var log_data_error string
 
 func Log_info(service string, fn string, msg string) {
-	log_data = log_data + time.Now().Format("15:04:05.0000") + "\tI\t" + service + "-" + fn + "\t" + msg + "\n"
+	log_data_info = log_data_info + time.Now().Format("15:04:05.0000") + "\tI\t" + service + "-" + fn + "\t" + msg + "\n"
 }
 func Log_warning(service string, fn string, msg string) {
-	log_data = log_data + time.Now().Format("15:04:05.0000") + "\tW\t" + service + "-" + fn + "\t" + msg + "\n"
+	log_data_warning = log_data_warning + time.Now().Format("15:04:05.0000") + "\tW\t" + service + "-" + fn + "\t" + msg + "\n"
 }
 func Log_error(service string, fn string, msg string) {
-	log_data = log_data + time.Now().Format("15:04:05.0000") + "\tE\t" + service + "-" + fn + "\t" + msg + "\n"
+	log_data_error = log_data_error + time.Now().Format("15:04:05.0000") + "\tE\t" + service + "-" + fn + "\t" + msg + "\n"
 }
 
 func Logs_Write(log_dir string, time_delay int) {
 
 	for {
-		if len(log_data) > 0 {
-			currentDate := time.Now().Format("2006-01-02")
-			fname := log_dir + "Log-" + currentDate + ".log"
+		if len(log_data_info) > 0 {
+			currentDate := time.Now().Format("20060102")
+			fname := log_dir + "Log-" + currentDate + "-info.log"
 			file, err := os.OpenFile(fname, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-			content := log_data
+			content := log_data_info
 			if err != nil {
 				fmt.Println("Error open file:", err)
 			} else {
@@ -34,10 +36,45 @@ func Logs_Write(log_dir string, time_delay int) {
 				if err != nil {
 					fmt.Println("Error writing to file:", err)
 				} else {
-					log_data = strings.Replace(log_data, content, "", -1)
+					log_data_info = strings.Replace(log_data_info, content, "", -1)
 				}
 			}
 		}
+
+		if len(log_data_warning) > 0 {
+			currentDate := time.Now().Format("20060102")
+			fname := log_dir + "Log-" + currentDate + "-warning.log"
+			file, err := os.OpenFile(fname, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+			content := log_data_warning
+			if err != nil {
+				fmt.Println("Error open file:", err)
+			} else {
+				_, err = file.WriteString(content)
+				if err != nil {
+					fmt.Println("Error writing to file:", err)
+				} else {
+					log_data_warning = strings.Replace(log_data_warning, content, "", -1)
+				}
+			}
+		}
+
+		if len(log_data_error) > 0 {
+			currentDate := time.Now().Format("20060102")
+			fname := log_dir + "Log-" + currentDate + "-error.log"
+			file, err := os.OpenFile(fname, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+			content := log_data_error
+			if err != nil {
+				fmt.Println("Error open file:", err)
+			} else {
+				_, err = file.WriteString(content)
+				if err != nil {
+					fmt.Println("Error writing to file:", err)
+				} else {
+					log_data_error = strings.Replace(log_data_error, content, "", -1)
+				}
+			}
+		}
+
 		time.Sleep(time.Duration(time_delay) * 1000 * time.Millisecond)
 	}
 }
